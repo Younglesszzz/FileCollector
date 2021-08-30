5b11dfc6134049c364d7e5e55b0fec80aafecb0c
Younglesszzz <571688981@qq.com>
2021-08-30 19:06:49 +0800

    Initial commit

---
+++ src/main/java/com/example/filecollector/FileCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.filecollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/filecollector/FileCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filecollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
